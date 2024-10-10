1089c97eb4d3e7f9487114bbea51a9d34e264cb9
Xander Roy <lunarlimpets@gmail.com>
2024-10-09 20:51:19 -0500

    add bar toggle

--- app.py
+++ app.py
@@ -2,51 +2,76 @@ import streamlit as st
 import plotly.express as px
 import pandas as pd
 
-#test
 grouped_df = pd.read_csv('grouped_lands.csv')
 
+st.title("Total Price of Basic Lands by Set")
 
-st.title("Market Share of Basic Lands by Set")
-
-
-fig = px.bar_polar(
-    grouped_df, 
-    theta='name', 
-    r='usd',
-    hover_data=['set', 'usd', 'usd_foil', 'usd_etched'],
-    color='set', 
-    height=1000,
+chart_type = st.radio(
+    "Select chart type:",
+    ('Polar Chart', 'Bar Chart')
 )
 
-fig.update_layout(
-    legend=dict(
-    #     orientation="v",
-    #     yanchor="bottom",
-    #     y=1,  
-    #     xanchor="right",
-    #     x=1,
-    #     title=dict(font=dict(size=14)), 
-    #     font=dict(size=12) 
-    ),
-    margin=dict(t=50, b=50, l=50, r=50)  
-)
-
-
-fig.update_layout(
-    title={
-        'text': "Price of Lands per Set<br><sup>sorted by initial release date</sup>",  
-        'font': {
-            'size': 24,          
-            # 'color': "Black"     
-        },
-        'x': 0.5,               
-        'xanchor': 'center',   
-        'y': 0.95,              
-        'yanchor': 'top'       
-    },
-    titlefont=dict(
-        family="Arial, sans-serif",  
+# Create Polar Chart
+if chart_type == 'Polar Chart':
+    fig = px.bar_polar(
+        grouped_df, 
+        theta='name', 
+        r='usd',
+        hover_data=['set', 'usd', 'usd_foil', 'usd_etched'],
+        color='set', 
+        height=1000,
     )
-)
 
-st.plotly_chart(fig)
+    fig.update_layout(
+        legend=dict(),
+        margin=dict(t=50, b=50, l=50, r=50)
+    )
+
+    fig.update_layout(
+        title={
+            'text': "Price of Lands per Set<br><sup>Data From Scryfall.com</sup><sup>sorted by initial release date</sup>",  
+            'font': {
+                'size': 24,          
+            },
+            'x': 0.5,               
+            'xanchor': 'center',   
+            'y': 0.95,              
+            'yanchor': 'top'       
+        },
+        titlefont=dict(
+            family="Arial, sans-serif",  
+        )
+    )
+
+    st.plotly_chart(fig)
+
+# Create Bar Chart
+elif chart_type == 'Bar Chart':
+    fig = px.bar(
+        grouped_df, 
+        x='name', 
+        y='usd', 
+        color='set', 
+        hover_data=['set', 'usd', 'usd_foil', 'usd_etched'],
+        height=1000
+    )
+
+    fig.update_layout(
+        title={
+            'text': "Price of Lands per Set<br><sup>Data From Scryfall.com</sup><sup>sorted by initial release date</sup>",  
+            'font': {
+                'size': 24,          
+            },
+            'x': 0.5,               
+            'xanchor': 'center',   
+            'y': 0.95,              
+            'yanchor': 'top'       
+        },
+        titlefont=dict(
+            family="Arial, sans-serif",  
+        ),
+        margin=dict(t=50, b=50, l=50, r=50),
+        legend=dict(orientation="h", yanchor="bottom", y=-0.2, xanchor="center", x=0.5)
+    )
+
+    st.plotly_chart(fig)
